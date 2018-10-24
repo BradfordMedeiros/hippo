e0b1ed6027bf4e8cb8b18fba64106d6f637ce35c
brad medeiros <bradmedeiros0@gmail.com>
2018-10-24 01:16:00 -0700

    add generate-manifest.py, and data sample folder

--- server/generate-manifest.py
+++ server/generate-manifest.py
@@ -1,6 +1,7 @@
-
 import argparse
 import sys
+import os
+import json
 
 def handle_parse_error(x):
 	raise Exception(x)
@@ -12,13 +13,27 @@ def parse_args(options):
 	options = parser.parse_args(options)
 	return vars(options)
 
-def generate_manifest(folder_path):
-	# get list of the folders
-	# for each folder add the tars to the thing
+def create_module_from_filename(filename, baseroute):
+	return { 'name': filename, 'path': os.path.join(baseroute,filename) }
+
+def generate_submanifest(directory, baseroute):
+	files = [x for x in os.walk(directory) if os.path.isfile][0][2]
+	return [create_module_from_filename(file, baseroute) for file in files]
+
+def generate_manifest(folder_path, baseroute):
+	tile_folder = os.path.join(folder_path, 'tiles')
+	module_folder = os.path.join(folder_path, 'modules')
+
 	manifests = { }
+	tile_data = generate_submanifest(tile_folder, baseroute)
+	module_data = generate_submanifest(module_folder, baseroute)
+
+	manifests['tiles'] = tile_data
+	manifests['modules'] = module_data
+
 	return manifests
 
 
-
 options = parse_args(sys.argv[1:])
-print options['data']
+manifest = generate_manifest(options['data'], 'http://blacksmith.io/hippo/')
+print json.dumps(manifest)

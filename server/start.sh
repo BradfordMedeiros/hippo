44af404968d33a9177744860dce15b6c9f065612
brad medeiros <bradmedeiros0@gmail.com>
2018-10-24 23:12:48 -0700

    parameterize content folder

--- server/start.sh
+++ server/start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 PORT=8000
+CONTENT_FOLDER="\/home\/brad\/automate\/hippo\/server\/data-sample";
 
 TEMPLATE_FILE=$(pwd)/nginx.conf.template
 GENERATED_FILE=$(pwd)/nginx.conf
 
-
-sed "s/{{PORT}}/$PORT/g" "$TEMPLATE_FILE" > "$GENERATED_FILE"
+sed -e "s/{{PORT}}/$PORT/g" -e "s/{{CONTENT_FOLDER}}/$CONTENT_FOLDER/g" "$TEMPLATE_FILE" > "$GENERATED_FILE"
 
 nginx -c "$GENERATED_FILE"
 

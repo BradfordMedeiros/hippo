211142cb71222db8929626d16f737c6f9e7784b5
brad medeiros <bradmedeiros0@gmail.com>
2018-10-24 23:17:47 -0700

    call generate manifest from start scripts

--- server/start.sh
+++ server/start.sh
@@ -5,9 +5,13 @@ CONTENT_FOLDER="\/home\/brad\/automate\/hippo\/server\/data-sample";
 
 TEMPLATE_FILE=$(pwd)/nginx.conf.template
 GENERATED_FILE=$(pwd)/nginx.conf
+MANIFEST_FILE=$(pwd)/data-sample/manifest.json
+
+python generate-manifest.py -d ./data-sample/ > "$MANIFEST_FILE"
 
 sed -e "s/{{PORT}}/$PORT/g" -e "s/{{CONTENT_FOLDER}}/$CONTENT_FOLDER/g" "$TEMPLATE_FILE" > "$GENERATED_FILE"
 
 nginx -c "$GENERATED_FILE"
 
 rm "$GENERATED_FILE"
+rm "$MANIFEST_FILE"

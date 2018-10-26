8e2f72d27b09cceec0331d179e860b60fd0fe01c
brad medeiros <bradmedeiros0@gmail.com>
2018-10-25 23:31:27 -0700

    remove tile functionality

--- client/utilities/automate/tiles/remove-tile.sh
+++ client/utilities/automate/tiles/remove-tile.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+tilename="$1"
 
+# GET THE DIRECTORY TO REMOVE BY SEARCHING LOCALLY INSTALLED TILES (JUST FOR CORRECTNESS -- will be nice to add in error messages, etc if this fails)
+tile_to_remove=$(./list-local-tiles.sh | grep -w "$tilename")
+tile_directory="../../../local-data/tiles/$tile_to_remove"
+
+rm -r "$tile_directory"

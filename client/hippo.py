6b6d8ef197fc65523966087ca74fec3946945522
brad medeiros <bradmedeiros0@gmail.com>
2018-10-27 22:48:44 -0700

    more work on surface

--- client/hippo.py
+++ client/hippo.py
@@ -17,9 +17,6 @@ def get_extension_directory(extension):
 	return os.path.join('./utilities', extension)
 
 def execute_script(extension, script_path):
-	print 'extension: ', extension
-	print 'script: ', script_path
-	print 'dir: ', get_extension_directory(extension)
 	call([os.path.join('.',script_path)], cwd=get_extension_directory(extension))
 
 args = sys.argv
@@ -27,7 +24,6 @@ surface = args[1]
 surface_args = args[2:]
 
 manifest = read_extension_manifest(surface)
-print 'manifest is: ', manifest
 command_params = surface_args[0]
 command = manifest[command_params]
 script_path = command['script']

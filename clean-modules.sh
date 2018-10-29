9247efd586a19e0bff7a6a3f6f9057964fd4f625
brad medeiros <bradmedeiros0@gmail.com>
2018-10-28 23:39:38 -0700

    install/remove data-folders

--- clean-modules.sh
+++ clean-modules.sh
@@ -11,3 +11,7 @@ rm -r server/data-sample/modules/*  2>/dev/null
 rm -r server/data-sample/styles/* 2>/dev/null
 rm -r server/data-sample/tiles/* 2>/dev/null
 
+rmdir server/data-sample/extensions
+rmdir server/data-sample/modules
+rmdir server/data-sample/styles
+rmdir server/data-sample/tiles
--- install-default-modules.sh
+++ install-default-modules.sh
@@ -5,5 +5,8 @@ cp -r extensions/extensions/extensions client/extensions/extensions
 cp -r extensions/extensions/pkg client/extensions/pkg
 
 #INSTALL MODULES TO SERVER
-
+mkdir -p server/data-sample/extensions
+mkdir -p server/data-sample/modules
+mkdir -p server/data-sample/styles
+mkdir -p server/data-sample/tiles
 

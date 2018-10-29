ec03de6bdd3f4b4dbd60f972159c34457d160253
brad medeiros <bradmedeiros0@gmail.com>
2018-10-28 23:53:53 -0700

    install extensions

--- install-default-modules.sh
+++ install-default-modules.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+function install_extension(){	
+   extension_name=$1
+   (cd extensions/extensions/extensions && tar -cvf "$extension_name.tar" *)
+   mv "extensions/extensions/extensions/$extension_name.tar" server/data-sample/extensions
+}
+
 #INSTALL MODULES TO CLIENT
 cp -r extensions/extensions/extensions client/extensions/extensions
 cp -r extensions/extensions/pkg client/extensions/pkg
@@ -10,3 +16,6 @@ mkdir -p server/data-sample/modules
 mkdir -p server/data-sample/styles
 mkdir -p server/data-sample/tiles
 
+
+
+install_extension extension 

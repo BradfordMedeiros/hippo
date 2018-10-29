4ed890cc407300d2479fca769beb0fa9306a856c
brad medeiros <bradmedeiros0@gmail.com>
2018-10-28 23:19:08 -0700

    install default extensions for client

--- install-default-modules.sh
+++ install-default-modules.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #INSTALL MODULES TO SERVER
-cp -r extensions/extensions client/extensions/extensions
-
+cp -r extensions/extensions/extensions client/extensions/extensions
+cp -r extensions/extensions/pkg client/extensions/pkg
 
 #INSTALL MODULES TO CLIENT
 

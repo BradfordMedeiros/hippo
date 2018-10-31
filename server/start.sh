63d0b9b07563a81d604d0cda85a9a9d399decdea
brad medeiros <bradmedeiros0@gmail.com>
2018-10-30 21:56:38 -0700

    template in nginc start parameters correctly

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PORT=8000
-CONTENT_FOLDER="\/home\/brad\/automate\/hippo\/server\/data-sample";
+CONTENT_FOLDER="$(pwd)/data-sample";
 MANIFEST_HOSTNAME="http://localhost:$PORT"
 
 TEMPLATE_FILE=$(pwd)/nginx.conf.template
@@ -10,7 +10,7 @@ MANIFEST_FILE=$(pwd)/data-sample/manifest.json
 
 python generate-manifest.py -d ./data-sample/ -b "$MANIFEST_HOSTNAME" > "$MANIFEST_FILE"
 
-sed -e "s/{{PORT}}/$PORT/g" -e "s/{{CONTENT_FOLDER}}/$CONTENT_FOLDER/g" "$TEMPLATE_FILE" > "$GENERATED_FILE"
+sed -e "s|{{PORT}}|$PORT|g" -e "s|{{CONTENT_FOLDER}}|$CONTENT_FOLDER|g" "$TEMPLATE_FILE" > "$GENERATED_FILE"
 
 nginx -c "$GENERATED_FILE"
 

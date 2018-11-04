2ae024bd2f93d29d005d3d7c03e6f6a98ae95321
brad medeiros <bradmedeiros0@gmail.com>
2018-11-03 22:34:28 -0700

    -

--- add-ons/extensions/services/add-service.sh
+++ add-ons/extensions/services/add-service.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+
+# Usage: 
+# hippo services install service_name /path/to/some/script.sh
+# hippo services uninstall service_name
+# hippo services list
+# need to check if /path/to/some/script.sh is executable
+
 echo "add service placeholder"
---
+++ add-ons/extensions/usb-exec/README.md
@@ -0,0 +1,9 @@
+## USB Execution
+
+Set a script to call when you plug in a usb.
+
+# Usage
+- hippo usb-exec add some-label /path/to/some/script.sh
+- hippo usb-exec remove some-label
+- hippo usb-exec list 
+

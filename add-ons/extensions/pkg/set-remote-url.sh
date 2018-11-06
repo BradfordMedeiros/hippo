5281511666ac072814e0d5bdbe7883018a8dd4a7
brad <bradmedeiros0@gmail.com>
2018-11-06 05:28:05 +0000

    set remote url

---
+++ add-ons/extensions/pkg/set-remote-url.sh
@@ -0,0 +1,13 @@
+REMOTE_URL=$1
+DESCRIPTION=$2
+
+if [[ -z "$REMOTE_URL" ]]; then
+	echo "remote url undefined"
+	exit 1
+fi 
+
+json_object="{ \"url\": \"$REMOTE_URL\", \"description\": \" $DESCRIPTION\"}"
+
+echo "$json_object" > ./data/remote/localserver
+
+./update-manifest.sh

55d9b933d93711a345123ddb127b5ffc6f00bdfa
brad medeiros <bradmedeiros0@gmail.com>
2018-12-21 01:09:26 -0800

    base route override

--- server/start.sh
+++ server/start.sh
@@ -9,13 +9,19 @@ CONTENT_FOLDER="$(pwd)/data-sample";
 if [[ ! -z "$1" ]]; then 
   MANIFEST_HOSTNAME="$1"
 else 
-  MANIFEST_HOSTNAME="http://$(get_ip):8000" 
+  MANIFEST_HOSTNAME="http://$(get_ip):80" 
 fi
 
 if [[ ! -z "$2" ]]; then
-  PORT="$2"
-else 
-  PORT="8000"
+   BASE_ROUTE="$2"
+else
+   BASE_ROUTE="/"
+fi
+
+if [[ ! -z "$3" ]]; then
+   PORT="$3"
+else
+   PORT=8000
 fi
 
 TEMPLATE_FILE=$(pwd)/nginx.conf.template
@@ -24,7 +30,11 @@ MANIFEST_FILE=$(pwd)/data-sample/manifest.json
 
 python generate-manifest.py -d ./data-sample/ -b "$MANIFEST_HOSTNAME" > "$MANIFEST_FILE"
 
-sed -e "s|{{PORT}}|$PORT|g" -e "s|{{CONTENT_FOLDER}}|$CONTENT_FOLDER|g" "$TEMPLATE_FILE" > "$GENERATED_FILE"
+sed \
+-e "s|{{BASE_ROUTE}}|$BASE_ROUTE|g"
+-e "s|{{PORT}}|$PORT|g" \
+-e "s|{{CONTENT_FOLDER}}|$CONTENT_FOLDER|g" \
+"$TEMPLATE_FILE" > "$GENERATED_FILE"
 
 nginx -c "$GENERATED_FILE"
 

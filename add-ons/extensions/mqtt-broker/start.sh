3674764cfe329f9a5a06b6ff63d032938ab7e055
brad medeiros <bradmedeiros0@gmail.com>
2019-01-07 23:24:18 -0800

    add start and stop scripts

--- add-ons/extensions/mqtt-broker/start.sh
+++ add-ons/extensions/mqtt-broker/start.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
 
+docker run -p 1883:1883 -d --name hippo.mqtt_broker eclipse-mosquitto:1.5.5
+
--- add-ons/extensions/mqtt-broker/stop.sh
+++ add-ons/extensions/mqtt-broker/stop.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 
+docker stop hippo.mqtt_broker && docker rm hippo.mqtt_broker

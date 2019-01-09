61fd489b1a0380855d4a8ad136ce5860118ffe8d
brad medeiros <bradmedeiros0@gmail.com>
2019-01-09 00:54:11 -0800

    super hacky script for midi json

--- add-ons/extensions/midi-listener/listen-midi-mqtt.sh
+++ add-ons/extensions/midi-listener/listen-midi-mqtt.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+
 
 # Mqtt topics look like this
 
@@ -9,16 +9,32 @@
 #   "ch": <string>, 
 #   "data": <string> 
 #}
-# @todo add the parsing logic to rip the above fields 
 
-aseqdump -p MPKmini2 | awk '
-/64/ {
-   print ("turn on")
-   system("mqtt --publish -t states/midi -m on")
-}
+aseqdump -p MPKmini2 | awk -v sq="'" '
 
-/60/{
-   print ("turn off")
-   system("mqtt --publish -t states/midi -m off")
-}
-'
+// {
+   split($0, comma_array, ",")
+    
+   data = ""
+   for (i =2 ; i <= length(comma_array); i++){      
+       data = data " " comma_array[i]
+       if (i != length(comma_array)){
+          data = data ","
+       }
+   }
+
+   split(comma_array[1], space_array, " ")
+   
+   source = space_array[1]
+   event = space_array[2]
+   for (i = 3; i < length(space_array); i++){
+      event = event " " space_array[i]
+   }
+  
+   channel = space_array[length(space_array)]  
+
+   json = "{"  "\"" "source" "\":" "\"" source "\","  "\"" "event" "\":" "\""  event "\"," "\"" "channel" "\":" "\""  channel "\","  "\"" "data" "\":" "\""  data "\"" "}"
+   message = "mqtt --publish -t states/midi -m " sq json sq
+   #print "message is " message
+   system(message)
+}'
